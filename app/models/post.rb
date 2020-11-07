0f4a32b2c47b4d1ca755207a0acc5c2511df9ba2
Omansh Mathur <omanshmathur1999@gmail.com>
2020-11-07 20:58:52 +0530

    Basic CRUD functionality

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+    validates :image, presence: true
+    has_attached_file :image
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+end
